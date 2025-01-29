edadff27eef5604a762b8d3d20bf29ec5bb8da4b
Dani <dan.avila7@gmail.com>
2025-01-28 22:28:16 -0500

    rollback

--- backend/src/routes/positionRoutes.ts
+++ backend/src/routes/positionRoutes.ts
@@ -1,11 +1,10 @@
 import { getCandidatesByPosition, getInterviewFlowByPosition, getAllPositions } from '../presentation/controllers/positionController';
-import { getPositionStats } from '../presentation/controllers/positionController';
+
 
 const router = require('express').Router();
 
 router.get('/', getAllPositions);
 router.get('/:id/candidates', getCandidatesByPosition);
 router.get('/:id/interviewflow', getInterviewFlowByPosition);
-router.get('/:id/stats', getPositionStats);
 
 export default router;
