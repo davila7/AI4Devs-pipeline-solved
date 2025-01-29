82b26fcec7e4fcdb22382deaa7dd07eb58d3a7b3
Dani <dan.avila7@gmail.com>
2025-01-28 22:26:09 -0500

    change test

--- backend/src/routes/positionRoutes.ts
+++ backend/src/routes/positionRoutes.ts
@@ -1,10 +1,11 @@
 import { getCandidatesByPosition, getInterviewFlowByPosition, getAllPositions } from '../presentation/controllers/positionController';
-
+import { getPositionStats } from '../presentation/controllers/positionController';
 
 const router = require('express').Router();
 
 router.get('/', getAllPositions);
 router.get('/:id/candidates', getCandidatesByPosition);
 router.get('/:id/interviewflow', getInterviewFlowByPosition);
+router.get('/:id/stats', getPositionStats);
 
 export default router;
